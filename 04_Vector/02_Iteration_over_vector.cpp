237db9b2030278c793c94a49adc2566aa6643dd8
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-06-26 00:17:25 +0500

    iteration over vector using for loop

---
+++ 04_Vector/02_Iteration_over_vector.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> b(5,10);
+    
+    // using for loop
+    for(int i=0;i<b.size();i++){
+        cout<<b[i]<<" ";
+    }
+
+    
+    return 0;
+}
