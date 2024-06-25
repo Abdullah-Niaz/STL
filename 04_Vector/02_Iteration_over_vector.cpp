7ff15bb4080e77958f1546ffbc8bd58d0478d67d
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-06-26 00:20:36 +0500

    iteration over vector using iterators

--- 04_Vector/02_Iteration_over_vector.cpp
+++ 04_Vector/02_Iteration_over_vector.cpp
@@ -8,7 +8,12 @@ int main(){
     for(int i=0;i<b.size();i++){
         cout<<b[i]<<" ";
     }
-
+    cout<<endl;
+    //  using iterators
+    for (auto i = b.begin(); i < b.end(); i++)
+    {
+        cout << *i << " "; // getting the element by derefrencing 
+    }
     
     return 0;
 }
